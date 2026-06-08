346ed3c5e87c0ca5a4086fc48c9ca84e7bf2bb28
Toxicity Analyzer Dev <developer@toxicityanalyzer.com>
2026-06-08 02:12:44 -0400

    Redesign dark mode switch: text with icons above, half-height rocker switch below

--- lib/widgets/dark_mode_switch.dart
+++ lib/widgets/dark_mode_switch.dart
@@ -15,106 +15,85 @@ class DarkModeSwitch extends StatelessWidget {
       builder: (context, child) {
         final isDarkMode = themeService.isDarkMode;
 
-        return GestureDetector(
-          onTap: () => themeService.toggleDarkMode(),
-          child: Container(
-            width: 100,
-            height: 44,
-            decoration: BoxDecoration(
-              color: isDarkMode
-                  ? Colors.black87
-                  : Colors.grey[300],
-              borderRadius: BorderRadius.circular(22),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.2),
-                  blurRadius: 4,
-                  offset: const Offset(0, 2),
-                ),
-              ],
-            ),
-            child: Stack(
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Text with icons above
+            Row(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                // Light mode label
-                Positioned(
-                  left: 12,
-                  top: 0,
-                  bottom: 0,
-                  child: Center(
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.light_mode,
-                          color: isDarkMode ? Colors.grey[600] : Colors.orange,
-                          size: 16,
-                        ),
-                        const SizedBox(width: 4),
-                        Text(
-                          l10n.lightMode,
-                          style: TextStyle(
-                            color: isDarkMode ? Colors.grey[600] : Colors.black87,
-                            fontSize: 12,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
+                Icon(
+                  Icons.light_mode,
+                  color: isDarkMode ? Colors.grey[600] : Colors.orange,
+                  size: 14,
+                ),
+                const SizedBox(width: 4),
+                Text(
+                  '${l10n.lightMode} / ${l10n.darkMode}',
+                  style: TextStyle(
+                    color: isDarkMode ? Colors.grey[400] : Colors.black87,
+                    fontSize: 10,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
-                // Dark mode label
-                Positioned(
-                  right: 12,
-                  top: 0,
-                  bottom: 0,
-                  child: Center(
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          l10n.darkMode,
-                          style: TextStyle(
-                            color: isDarkMode ? Colors.blue[300] : Colors.grey[600],
-                            fontSize: 12,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        const SizedBox(width: 4),
-                        Icon(
-                          Icons.dark_mode,
-                          color: isDarkMode ? Colors.blue[300] : Colors.grey[600],
-                          size: 16,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                // Rocker knob
-                AnimatedPositioned(
-                  duration: const Duration(milliseconds: 200),
-                  curve: Curves.easeInOut,
-                  left: isDarkMode ? 56 : 4,
-                  top: 4,
-                  bottom: 4,
-                  child: Container(
-                    width: 36,
-                    height: 36,
-                    decoration: BoxDecoration(
-                      color: isDarkMode ? Colors.blueGrey[800] : Colors.white,
-                      shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.3),
-                          blurRadius: 3,
-                          offset: const Offset(0, 2),
-                        ),
-                      ],
-                    ),
-                  ),
+                const SizedBox(width: 4),
+                Icon(
+                  Icons.dark_mode,
+                  color: isDarkMode ? Colors.blue[300] : Colors.grey[600],
+                  size: 14,
                 ),
               ],
             ),
-          ),
+            const SizedBox(height: 4),
+            // Half-height rocker switch below
+            GestureDetector(
+              onTap: () => themeService.toggleDarkMode(),
+              child: Container(
+                width: 60,
+                height: 22,
+                decoration: BoxDecoration(
+                  color: isDarkMode
+                      ? Colors.black87
+                      : Colors.grey[300],
+                  borderRadius: BorderRadius.circular(11),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 3,
+                      offset: const Offset(0, 1),
+                    ),
+                  ],
+                ),
+                child: Stack(
+                  children: [
+                    // Rocker knob
+                    AnimatedPositioned(
+                      duration: const Duration(milliseconds: 200),
+                      curve: Curves.easeInOut,
+                      left: isDarkMode ? 32 : 2,
+                      top: 2,
+                      bottom: 2,
+                      child: Container(
+                        width: 18,
+                        height: 18,
+                        decoration: BoxDecoration(
+                          color: isDarkMode ? Colors.blueGrey[800] : Colors.white,
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.3),
+                              blurRadius: 2,
+                              offset: const Offset(0, 1),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         );
       },
     );
